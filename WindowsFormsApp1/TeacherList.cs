41c9c495375119c771bab543af1036836e17b366
zaubenko <89977826+zaubenko@users.noreply.github.com>
2021-11-24 23:52:09 +0200

    Add files via upload

--- WindowsFormsApp1/TeacherList.cs
+++ WindowsFormsApp1/TeacherList.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace WindowsFormsApp1
 {
     static class TeacherList
     {
         public static List<Teacher> teacherlist = new List<Teacher>();
+        public static string directory = PathToFiles();
+        public static string SaveDirectory = PathToFiles() + "TeachersList.json";
+
+        public static string PathToFiles()
+        {
+            string file = Environment.CurrentDirectory;
+            string sendfile = file.Substring(0, file.LastIndexOf("bin")) + "Info" + "\\";
+            return sendfile;
+        }
+        public static void Load()
+        {
+            teacherlist = JsonConvert.DeserializeObject<List<Teacher>>(File.ReadAllText(SaveDirectory));
+        }
     }
 }
